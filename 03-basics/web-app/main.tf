9fddcd51f4cadca869ccbba04677bbe3f8ef563d
austin <aokojie@gmail.comclear>
2025-01-20 19:30:45 -0500

    Commented out 2nd EC2 instance

--- 03-basics/web-app/main.tf
+++ 03-basics/web-app/main.tf
@@ -32,16 +32,16 @@ resource "aws_instance" "instance_1" {
               EOF
 }
 
-resource "aws_instance" "instance_2" {
-  ami             = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.instances.name]
-  user_data       = <<-EOF
-              #!/bin/bash
-              echo "Hello, World 2" > index.html
-              python3 -m http.server 8080 &
-              EOF
-}
+#resource "aws_instance" "instance_2" {
+#  ami             = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
+#  instance_type   = "t2.micro"
+#  security_groups = [aws_security_group.instances.name]
+#  user_data       = <<-EOF
+#              #!/bin/bash
+#              echo "Hello, World 2" > index.html
+#              python3 -m http.server 8080 &
+#             EOF
+#}
 
 resource "aws_s3_bucket" "bucket" {
   bucket_prefix = "devops-directive-web-app-data"
