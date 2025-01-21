b30e3af2389405cdf9a2e3209c3e61236f974383
austin <aokojie@gmail.comclear>
2025-01-20 19:35:55 -0500

    change bucket count - 02

--- 02-overview/main.tf
+++ 02-overview/main.tf
@@ -14,4 +14,5 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
   instance_type = "t2.micro"
+  count         = 4 
 }
